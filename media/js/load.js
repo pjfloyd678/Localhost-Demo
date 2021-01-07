c092dfabb315f4fac4e76bab6432bbc38f089485
Peter Floyd <peterjfloyd@gmail.com>
2021-01-07 12:04:46 -0500

    Initial clean-up of non-essential items

--- media/js/load.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function () {
-    var maxCount = 100;
-    var interval = Math.ceil(maxCount/10);
-    for (var c=0; c<maxCount; c=c+interval) {
-        var range = c.toString() + "-" + ((c+interval)-1).toString();
-        var value = (c+1).toString() + "-" + (c+interval).toString();
-        console.log(value + " --> " + range);
-    }
-});
