94aa9d6631adaf8c9be618204ecf0568e8fd2045
Peter Floyd <pfloyd@healthloyalty.com>
2023-03-10 09:20:41 -0500

    Fix unauthorized user.

--- auth/authorize.php
+++ auth/authorize.php
@@ -18,26 +18,31 @@ if ( !empty( $_POST ) ) {
     $e    = filter_input( INPUT_POST, "emailaddress" );
     $p    = filter_input( INPUT_POST, "password" );
     $data = $cUser->getByEmail( $e );
-    $r    = $data[ 'response' ][ 0 ];
-    if ( empty( $r ) === 0 ) {
-        // Invalid User - Error!
-        echo "Invalid User - Bad Email!";
-        die(1);
-    }
-    $user = $data[ 'response' ][ 0 ];
-    if ( $user[ 'emailaddress' ] === $e ) {
-        $check = $passHash->CheckPassword( $p, $user[ 'password' ] );
-        if ( !$check ) {
-            // Invalid Password - Error
-            echo "Invalid User - Bad Password!";
+    if ( isset( $data[ 'response' ][ 0 ] ) ) {
+        $r    = $data[ 'response' ][ 0 ];
+        if ( empty( $r ) === 0 ) {
+            // Invalid User - Error!
+            echo "Invalid User - Bad Email!";
             die(1);
         }
-        // Valid Email and Password - Continue
-        $_SESSION[ 'loggedIn' ] = true;
-        $token = uniqid();
-        $_SESSION[ 'token' ]    = $token;
-        redirect("index.php");
-        exit();
+        $user = $data[ 'response' ][ 0 ];
+        if ( $user[ 'emailaddress' ] === $e ) {
+            $check = $passHash->CheckPassword( $p, $user[ 'password' ] );
+            if ( !$check ) {
+                // Invalid Password - Error
+                echo "Invalid User - Bad Password!";
+                die(1);
+            }
+            // Valid Email and Password - Continue
+            $_SESSION[ 'loggedIn' ] = true;
+            $token = uniqid();
+            $_SESSION[ 'token' ]    = $token;
+            redirect("index.php");
+            exit();
+        }
+    } else {
+        echo "Invalid User - User not found";
+        die(1);
     }
 }
 echo "Cannot process - No Post Data";
--- info.php
+++ info.php
@@ -5,5 +5,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+?>
+<pre>
+<?php print_r( $_SERVER ); ?>
+</pre>
+<?php
+die(-1);
 phpinfo();
